b448a6276cf0c5ee725ec7ac75db720a866669bf
rootc137 <pessicksanchez@gmail.com>
2023-08-18 09:41:24 +0200

    meta description, design account..

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Header} from "./Components/Header/Header";
